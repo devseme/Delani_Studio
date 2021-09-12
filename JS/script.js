10fb9ad1973b1e3a99bf5442fdbfccf63308856b
devseme <ian.ochenge@student.moringaschool.com>
2021-09-12 23:49:33 +0300

    more modifications

--- JS/script.js
+++ JS/script.js
@@ -12,3 +12,33 @@ $(document).ready(function(){
       }
     })
     });
+    $(document).ready(function(){
+        $("#designpic").click(function(){
+          $("#designpic").slideDown('1500').hide('1200');
+          $("#design").show('1500');
+        });
+        $("#design").click(function(){
+          $("#design").slideUp('1500');
+          $("#designpic").slideDown('1500');
+        });
+      });
+      $(document).ready(function(){
+        $("#dev-icon").click(function(){
+          $("#dev-icon").slideDown('1500').hide('1000');
+          $("#development").show('1500');
+        });
+        $("#development").click(function(){
+          $("#development").slideUp('1500');
+          $("#dev-icon").slideDown('1500');
+        });
+      });
+      $(document).ready(function(){
+        $("#products-icon").click(function(){
+          $("#products-icon").slideDown('1500').hide('1000');
+          $("#products").show('1500');
+        });
+        $("#products").click(function(){
+          $("#products").slideUp('1500');
+          $("#products-icon").slideDown('1500');
+        });
+      });
